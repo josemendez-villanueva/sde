5a97942f9ba43d9acea728e495077724e2b57184
josemendez-villanueva <joseph13.mv@gmail.com>
2021-07-03 14:05:06 -0700

    Cleaned up extra code and comments

--- bounded_sde_skel.py
+++ bounded_sde_skel.py
@@ -1,8 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from numpy.lib.type_check import real
 from scipy.linalg import toeplitz
-from math import isclose
 
 
 class Values(object):
@@ -34,14 +32,14 @@ class Values(object):
         random_vector = np.reshape(random_values, (self.N, 1))
 
         mm = self.sigma * np.matmul(upper_triangular, random_vector)        #Added grav here                                                            
-        solution_matrix = mm + column_vector + self.grav                    #############grav constant added
-        solution_matrix = np.insert(solution_matrix, 0, self.v_0)                #The third parameter will be the initial value added, the second paramter is for position in array
+        solution_matrix = mm + column_vector + self.grav                   
+        solution_matrix = np.insert(solution_matrix, 0, self.v_0)           #The third parameter will be the initial value added, the second paramter is for position in array
         solution_matrix = solution_matrix.transpose()
        
         trap_matrix = (self.sigma * (1 + np.exp(self.b * self.deltat))  / 2   ) * upper_triangular
         matrix_multiplication = np.matmul(trap_matrix, random_vector )
         
-        solution =  matrix_multiplication + column_vector - self.grav * self.deltat       #############grav constant added
+        solution =  matrix_multiplication + column_vector - self.grav * self.deltat       #grav constant added
         solution = np.insert(solution, 0, self.v_0)
         solution = solution.transpose()
 
@@ -51,32 +49,21 @@ class Values(object):
         N = self.N
         x = np.zeros( (N + 1, 1) )
         solution = self.main_equation()
-        dummy = 56 #random number that it should to actually hit
+  
         for i in range(1, N+1): 
             x[i] = np.round(x[i - 1] + ( solution[i-1] * self.deltat), 4)
-
-            tol = .00005
-
             if x[i] <= 0.1000 and x[i] >= -0.1000 :
-                
-                # dummy vairable is created in elif statements below but is used here in order to
-                #skip an iteration that is created there....
-
-                # if dummy == x[i]:
-                #     continue
-                # else:
                 x[i] = x[i - 1] + ( solution[i] * self.deltat) 
 
             elif x[i] < -0.1000:
                 x[i] = np.round(x[i - 1] + ( solution[i-1] * self.deltat), 4)
                 #Make the new dt
                 dt =  np.abs(((-.1000 -x[i-1]) / solution[i-1]))
-                #Redifine x[i] for when it is at the boundary
+                #Redefine x[i] for when it is at the boundary
                 x[i] = np.round(x[i - 1] + ( solution[i-1] * dt), 4)
                 #Now define new Initial Values since reflecting back
                 self.v0 = -solution[i-1]
                 solution = self.main_equation()
-
 
             elif x[i] > 0.1000:
                 x[i] = np.round(x[i - 1] + ( solution[i-1] * self.deltat), 4)
@@ -85,79 +72,8 @@ class Values(object):
                 #Step it at the new founded time t since that wil give you the point in wihcihc it crosses or grid size
                 self.v0 = -solution[i-1]
                 solution = self.main_equation()
-
-
-            
-# deltat = x[j-1] / solution[i-1 ]
-# let x[j] be zero and find deltat for when it crosses over
-# save that time step anfd swap it out, it will change the gri but it doesnt matter
-#then set the x0 to 0 and that v0 flipped and continue from there
-
-
-
-                # h = 5000
-                # list = np.zeros((h+1, 1))
-                # time = np.linspace(solution[i-1], solution[i], h)
-                # z = int(h/2)
-                # for j in range(1, h):
-                #     list[j] = np.round(x[i-1] + (time[j])*(self.T/h)*j, 3)
-
-
-                # for g in range(1,z):
-                #     if list[z] < -0.1000:
-                #         #only thing changing is the list[z,g] indexing
-                #         list[z - g] = np.round(x[i-1] + (time[(z - g)])*(self.T/h)*(z - g), 3)
-                #         if isclose(list[z-g], -0.1000, rel_tol= tol):
-                #             x[i] = np.round(x[i-1] + (list[z-g] * (self.T/h)*(z - g)), 3)
-                #             x[i+1] = np.round(x[i] + (-list[z-g] * (self.deltat - (self.T/h)*(z - g))), 3)
-                #             self.v0 = -list[z-g]
-                #             solution = self.main_equation()
-                #             dummy = x[i+1]
-                            
-                #     else:
-                #         list[z + g] = np.round(x[i-1] + (time[z + g])*(self.T/h)*(z + g), 3)
-                #         if isclose(list[z+g], -0.1000, rel_tol= tol):
-                #             x[i] = np.round(x[i-1] + (list[z+g] * (self.T/h)*(z + g)), 3)
-                #             x[i+1] = np.round(x[i] + (-list[z+g] * (self.deltat - (self.T/h)*(z + g))), 3)
-                #             self.v0 = -list[z+g]
-                #             solution = self.main_equation()
-                #             dummy = x[i+1]
-                            
-                                  
-
-            # elif x[i] > 0.1000:
-            #     h = 5000
-            #     list1 = np.zeros((h+1, 1))
-            #     time1 = np.linspace(solution[i-1], solution[i], h)
-            #     z = int(h/2)
-            #     for j in range(1, h):
-            #         list1[j] = np.round(x[i-1] + (time1[j])*(self.T/h)*j, 3)
-            #     for g in range(1,z):
-            #         if list1[z] < 0.1000:
-            #             list1[z + g] = np.round(x[i-1] + (time1[z+g])*(self.T/h)*(z+g), 3)
-            #             if isclose(list1[z+g], 0.1000, rel_tol= tol):
-            #                 x[i] = np.round(x[i-1] + (list1[z+g] * (self.T/h)*(z+g)), 3)
-            #                 x[i+1] = np.round(x[i] + (-list1[z+g] * (self.deltat - (self.T/h)*(z+g))), 3)
-            #                 self.v0 = -list1[z+g]
-            #                 solution = self.main_equation()
-            #                 dummy = x[i+1]
-            #         else:
-            #             list1[z - g] = np.round(x[i-1] + (time1[z-g])*(self.T/h)*(z-g), 3)
-            #             if isclose(list1[z-g], 0.1000, rel_tol= tol):
-            #                 x[i] = np.round(x[i-1] + (list1[z-g] * (self.T/h)*(z - g)), 3)
-            #                 x[i+1] = np.round( x[i] + (-list1[z-g] * (self.deltat - (self.T/h)*(z - g))), 3)
-            #                 self.v0 = -list1[z-g]
-            #                 solution = self.main_equation()
-            #                 dummy = x[i+1]
-                            
+                         
         return x
-
-
-            # incorporate intersection...(look where it hit the boundary) then at that time make it reflect back and hit ht enext time grid
-            #go from un-uniform grid to uniform grid for that time step
-            #####This will be the test implimentation to check to see if this works####
-
-
    
 class Graph(Values):   
         def graph(self):
@@ -181,9 +97,7 @@ class Graph(Values):
             plt.plot(meshpoints, y1, color = 'silver', linestyle = 'dashed', linewidth = 1.15)        
 
             plt.legend()
-            # plt.tight_layout()
             plt.show()
-
 
 def main():
     N = (1000)
@@ -194,4 +108,4 @@ def main():
     plotting_class.graph()
 
 if __name__ == "__main__":
-    main()                                         #since running program as main file
+    main()                                                            #since running program as main file
